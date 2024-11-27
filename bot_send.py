f71675c93a4c58f5b3114b6f6f86a08712120fd6
yashima-ksk <143680607+yashima-ksk@users.noreply.github.com>
2024-11-27 13:44:50 +0900

    env sub

---
+++ bot_send.py
@@ -0,0 +1,30 @@
+import discord
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+#envファイルからTOKEN , ID の読み込み
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))  #https://discord.com/channels/{サーバーID}/{チャンネルID}
+
+
+#clientの作成と、その権限の付与
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"ログイン完了 : {client.user}")
+    #送信先のチャンネルの取得
+    channel = client.get_channel(CHANNEL_ID)
+    #正常にチャンネル取得できればメッセージ送信
+    if channel:
+        now = datetime.now()
+        format_date = now.strftime("%m/%d %H:%M:%S")
+        await channel.send(f"【{format_date}】 test massage")
+    await client.close()
+
+client.run(TOKEN)
